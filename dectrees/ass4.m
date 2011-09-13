4f0057f684a98e3d52a81cf38702c1b909906c97
Carl-Oscar Erneholm <coer@csc.kth.se>
2011-09-13 20:48:43 +0200

    ASSES 4 EVERYONE!!!!

---
+++ dectrees/ass4.m
@@ -0,0 +1,11 @@
+function [err] = ass4(data, test)
+err = [];
+for frac = (3:8)/10
+    [n,m]=size(data);
+    p=randperm(n);
+    data_new=data(p(1:floor(n*frac)),:);
+    data=data(p(floor(n*frac)+1:n),:);
+    T1=build_tree(data_new);
+    T1p=prune_tree(T1,data);
+    err= [err calculate_error(T1p, test)];
+end
