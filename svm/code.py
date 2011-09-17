349913b580569905b6d2a8c8270506e8b977cf18
Martin Nycander <martin.nycander@gmail.com>
2011-09-17 16:51:00 +0200

    BASE CODE! AND TODOS! DONT FORGET THE TODOS!!!!11

---
+++ svm/code.py
@@ -0,0 +1,11 @@
+from cvxopt.solvers import qp
+from cvxopt.base import matrix
+
+import numpy, pylab, random, math
+
+# TODO: A suitable kernel function
+# TODO: Build P matrix from a given set of data points
+# TODO: Build the q vector, G matrix and the h vector
+# TODO: Call qp
+# TODO: Pick out the non-zero alpha values
+# TODO: Implement the indicator function
