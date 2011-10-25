2d1b5242b215a54fc63ecd8c3d03c52beaa10282
Carl-Oscar Erneholm <coer@csc.kth.se>
2011-10-25 21:10:30 +0200

    OH WHAT THE FUCKING EVER WHO GIVES A SHIT.

--- rl/qlearning.py
+++ rl/qlearning.py
@@ -1,62 +1,31 @@
 import random
-
+import numpy
+import pylab
 import environment
+import pprint
 
-# Constants for Q-learning
-learningRate = 0.9 # 0 < n < 1
-discountFactor = 0.9 # 0 < g < 1
-chooseBestFactor = 0.75 # 0 < e < 1
-
-# Variables for current experiment
-states = range(16)
-actions = range(4)
-
-# Initialize Q(s, a) arbitrarily for all s and a
-Q = [[random.normalvariate(0, 2) for a in range(len(actions))] for s in range(len(states))]
-
-# Initialize s
-state = random.randrange(0, len(state))
-
-environment = Environment(state)
-
-# Repeat for each step: (Until T steps)
-for t in range(T):
-	# * Choose a from s using e-greedy policy based on Q(s, a)
-	action = egreedy(chooseBestFactor, lamdba(a): Q(state, a), actions)
-	# * Take action a, observe r, and next state s'
-	(newState, reward) = environment.go(action)
-	# * Update Q(s, a) <- Q(s, a) + n[r + gamma * max_(a') Q(s', a') - Q(s, a)]
-	maxFutureValue = argmax(lambda(newAction): Q(newState, newAction), actions)
-	Q[state][action] += learningRate*(reward + discountFactor*maxFutureValue - Q(state, action))
-	# * Replace s with s'
-	state = newState
-
-# Patch together all walking animations from all states
-comicbook = []
-for state in range(0, 16) :
-	test = []
-	test.append(state)
-	# Find 8 following states
-	for f in range(0, 8) :
-		state = egreedy(chooseBestFactor, lamdba(a): Q(state, a), actions)
-		test.append(state)
-	# Show comic
-	comic = numpy.concatenate([images[i] for i in test], axis=1)
-	if len(comicbook) == 0:
-		comicbook = comic
-	else:
-		comicbook = numpy.concatenate((comicbook, comic), axis=0)
-	
-# Show comics for all states
-pylab.imshow(comicbook)
-pylab.show()
-
+images = (pylab.imread('step1.png'),
+          pylab.imread('step2.png'),
+          pylab.imread('step3.png'),
+          pylab.imread('step4.png'),
+          pylab.imread('step5.png'),
+          pylab.imread('step6.png'),
+          pylab.imread('step7.png'),
+          pylab.imread('step8.png'),
+          pylab.imread('step9.png'),
+          pylab.imread('step10.png'),
+          pylab.imread('step11.png'),
+          pylab.imread('step12.png'),
+          pylab.imread('step13.png'),
+          pylab.imread('step14.png'),
+          pylab.imread('step15.png'),
+          pylab.imread('step16.png'))
 
 def egreedy(e, f, args):
-	if random() < e
-		return args[random.randint(0, len[args]-1)]
-	else
-		return argmax f, args
+	if random.random() < e:
+		return args[random.randint(0, len(args)-1)]
+	else:
+		return argmax(f, args)
 
 
 # Utility function argmax
@@ -69,3 +38,54 @@ def argmax(f, args):
 			m = v
 			mi = i
 	return mi
+# Constants for Q-learning
+learningRate = 0.9 # 0 < n < 1
+discountFactor = 0.9 # 0 < g < 1
+chooseBestFactor = 0.1 # 0 < e < 1
+
+# Variables for current experiment
+states = range(16)
+actions = range(4)
+
+# Initialize Q(s, a) arbitrarily for all s and a
+Q = [[random.normalvariate(0, 2) for a in range(len(actions))] for s in range(len(states))]
+
+# Initialize s
+state = random.randrange(0, len(states))
+
+environment = environment.Environment(state)
+
+# Repeat for each step: (Until T steps)
+T = 1000
+for t in range(T):
+	# * Choose a from s using e-greedy policy based on Q(s, a)
+	action = egreedy(chooseBestFactor, lambda(a): Q[state][a], actions)
+	# * Take action a, observe r, and next state s'
+	(newState, reward) = environment.go(action)
+	# * Update Q(s, a) <- Q(s, a) + n[r + gamma * max_(a') Q(s', a') - Q(s, a)]
+	maxFutureValue = argmax(lambda(newAction): Q[newState][newAction], actions)
+	Q[state][action] += learningRate*(reward + discountFactor*maxFutureValue - Q[state][action])
+	# * Replace s with s'
+	state = newState
+
+pprint.pprint(Q)
+
+# Patch together all walking animations from all states
+comicbook = []
+for state in range(0, 16) :
+	test = []
+	test.append(state)
+	# Find 8 following states
+	for f in range(0, 8) :
+		state = egreedy(0, lambda(a): Q[state][a], actions)
+		test.append(state)
+	# Show comic
+	comic = numpy.concatenate([images[i] for i in test], axis=1)
+	if len(comicbook) == 0:
+		comicbook = comic
+	else:
+		comicbook = numpy.concatenate((comicbook, comic), axis=0)
+	
+# Show comics for all states
+pylab.imshow(comicbook)
+pylab.show()
