ab710b0fd04af5bd2acedeced69d37ce17c1ba1b
Carl-Oscar Erneholm <coer@csc.kth.se>
2011-10-25 20:42:30 +0200

    egreedy DONE????!?!?!? I THINK SO!!!1

--- rl/qlearning.py
+++ rl/qlearning.py
@@ -20,3 +20,37 @@ for t in range(T):
 	# * Update Q(s, a) <- Q(s, a) + n[r + gamma * max_(a') Q(s', a') - Q(s, a)]
 
 	# * Replace s with s'
+
+
+def egreedy(e, f, args):
+	if random() < e
+		return args[random.randint(0, len[args]-1)]
+	else
+		return argmax f, args
+
+
+# Utility function argmax
+def argmax(f, args):
+	mi = None
+	m = -1e10
+	for i in args:
+		v = f(i)
+		if v > m:
+			m = v
+			mi = i
+	return mi
+# Policy iteration
+policy = [None for s in trans]
+value = [0 for s in trans]
+for p in range(100):
+	for s in range(len(policy)):
+		policy[s] = argmax(
+			lambda(a): 
+				rew[s][a] + gamma*value[trans[s][a]], 
+			range(4))
+	
+	for s in range(len(value)):
+		a = policy[s]
+		value[s] = rew[s][a] + gamma*value[trans[s][a]]
+
+
