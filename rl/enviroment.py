4dfaf74879759bd0c1caa3deccabab0a33b72632
Carl-Oscar Erneholm <coer@csc.kth.se>
2011-10-25 20:19:08 +0200

    Environment.py DONE??!?!?

--- rl/enviroment.py
+++ rl/enviroment.py
@@ -1,10 +1,3 @@
-class Environment
-	def __init__(self, state=0):
-		self.state= state
-		self.trans = #TODO
-		self.rew = #TODO
-
-	def go(self, a):
 # Reward the final step for walking forward
 # Punish the final step for walking backwards
 goodbad =   ((0, 0, 0, 0,),
@@ -69,3 +62,31 @@ imfalling = ((0, 0, 0, 0,),
 			 (-1, 0, -1, 0,),
 			 (0, 0, 0, 0,))
 
+
+trans = ((1, 3, 4, 12),
+		 (0, 2, 5, 13),
+		 (3, 1, 6, 14),
+		 (2, 0, 7, 15),
+		 (5, 7, 0, 8),
+		 (4, 6, 1, 9),
+		 (7, 5, 2, 10),
+		 (6, 4, 3, 11),
+		 (9, 11, 12, 4),
+		 (8, 10, 13, 5),
+		 (11, 9, 14, 6),
+		 (10, 8, 15, 7),
+		 (13, 15, 8, 0),
+		 (12, 14, 9, 1),
+		 (15, 13, 10, 2),
+		 (14, 12, 11, 3))
+
+class Environment:
+	def __init__(self, state=0):
+		self.state= state
+		self.trans = trans
+		self.rew = [[imfalling[row][i]+goodbad[row][i]+norollerblade[row][i] for i in range(len(imfalling[row]))] for row in range(len(imfalling))]
+
+	def go(self, a):
+		r = self.rew[self.state][a]
+		self.state = self.trans[self.state][a]
+		return self.state, r
