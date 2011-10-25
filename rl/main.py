acb66612254e3b6a1cc9b683433ce091baf91297
Martin Nycander <martin.nycander@gmail.com>
2011-10-25 15:50:06 +0200

    TESTING FOR ZER MASSES!!111
    Signed-off-by: Martin Nycander <martin.nycander@gmail.com>

--- rl/main.py
+++ rl/main.py
@@ -1,3 +1,26 @@
+# From cartoon.py
+import numpy
+import pylab
+
+images = (pylab.imread('step1.png'),
+          pylab.imread('step2.png'),
+          pylab.imread('step3.png'),
+          pylab.imread('step4.png'),
+          pylab.imread('step5.png'),
+          pylab.imread('step6.png'),
+          pylab.imread('step7.png'),
+          pylab.imread('step8.png'),
+          pylab.imread('step9.png'),
+          pylab.imread('step10.png'),
+          pylab.imread('step11.png'),
+          pylab.imread('step12.png'),
+          pylab.imread('step13.png'),
+          pylab.imread('step14.png'),
+          pylab.imread('step15.png'),
+          pylab.imread('step16.png'))
+
+# Policy iteration variables
+
 gamma = 0.9
 rew = ( # CO SKRIVER DET HÄR
 		)
@@ -32,6 +55,26 @@ for p in range(100):
 	for s in range(len(value)):
 		a = policy[s]
 		value[s] = rew[s][a] + gamma*value[trans[s][a]]
+
+# Patch together all walking animations from all states
+comicbook = []
+for state in range(0, 16)
+
+	test = []
+	test.append(state)
+
+	# Find 5 following states
+	for f in range(0, 5)
+		state = value[state]
+		test.append(state)
+	
+	# Show comic
+	comic = numpy.concatenate([images[i] for i in test], axis=1)
+	comicbook = numpy.concatenate((comicbook, comic), axis=0)
+
+# Show comics for all states
+pylab.imshow(comicbook)
+pylab.show()
 
 # Utility function argmax
 def argmax(f, args):
